334e23e2f706f839c7454e337739905684fdb9e6
arthur-ia <aleshashinomiya@gmail.com>
2025-02-04 22:40:46 +0700

    p

--- resources/views/backend/navbar.blade.php
+++ resources/views/backend/navbar.blade.php
@@ -79,9 +79,9 @@
                             <span class="d-none d-lg-inline-flex">Yuki Suou</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="{{ route('profile') }}" class="dropdown-item">My Profile</a>
+                            <a href="" class="dropdown-item">My Profile</a>
                             <a href="#" class="dropdown-item">Settings</a>
-                            <form action="{{ route('logout') }}" method="POST">
+                            <form action="" method="POST">
                                 @csrf
                             <button class="dropdown-item">Log Out</button>
                             </form>
